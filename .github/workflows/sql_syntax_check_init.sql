a54ecf95360951dbc944026f90e7b7848791da5c
wrongecho <32306651+wrongecho@users.noreply.github.com>
2024-03-17 14:33:55 +0000

    Update sql_syntax_check_init.sql

--- .github/workflows/sql_syntax_check_init.sql
+++ .github/workflows/sql_syntax_check_init.sql
@@ -1,5 +1,1 @@
--- Initialize the database
-mysql_install_db --user=mysql --auth-root-authentication-method=normal
-
--- Set the root password
-mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+.
